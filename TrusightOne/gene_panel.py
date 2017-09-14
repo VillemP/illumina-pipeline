f45ba9dba96d55608c438d23acf8a78bc8cae14f
Villem Pata <villem.pata@gmail.com>
2017-09-15 00:31:54 +0300

    Gene panel objects for future development.

---
+++ TrusightOne/gene_panel.py
@@ -0,0 +1,15 @@
+class genepanel:
+    def __init__(self, name, panel_id, json):
+        self.name = name
+        self.panel_id = panel_id
+        self.json = json
+        self.genes = list()
+        self.genes_json = None
+
+    def add_genes(self, json):
+        self.genes_json = json
+        for gene in json:
+            self.genes.append(gene)
+
+    def __str__(self):
+        return "Name={0}, Id={1}, JSON={2}".format(self.name, self.panel_id, self.json)
